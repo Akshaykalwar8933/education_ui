87db2afcd43d5150283e61a18f0169a8e6bbe959
Akshay <akshayjaiswal1618@gmail.com>
2023-09-22 14:22:01 +0530

    home scrren and rename of images

--- lib/Home_page.dart
+++ lib/Home_page.dart
@@ -8,6 +8,60 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List catNames = [
+    "category",
+    "Classes",
+    "Free Cource",
+    "BookStore",
+    "Live Cource",
+    "LeaderBoard"
+  ];
+  List catColors = [
+    const Color(0xffFFCF2F),
+    const Color(0xFF6FE08D),
+    const Color(0xFF61BDFD),
+    const Color(0xFFFC7C7F),
+    const Color(0xFFCB84FB),
+    const Color(0xFF78E667),
+  ];
+  List catIcons = [
+    const Icon(
+      Icons.category,
+      color: Colors.white,
+      size: 30,
+    ),
+    const Icon(
+      Icons.video_library,
+      color: Colors.white,
+      size: 30,
+    ),
+    const Icon(
+      Icons.assessment,
+      color: Colors.white,
+      size: 30,
+    ),
+    const Icon(
+      Icons.store,
+      color: Colors.white,
+      size: 30,
+    ),
+    const Icon(
+      Icons.play_circle_fill,
+      color: Colors.white,
+      size: 30,
+    ),
+    const Icon(
+      Icons.emoji_events,
+      color: Colors.white,
+      size: 30,
+    )
+  ];
+  List imgList = [
+    "Flutter",
+    "React Native",
+    "Python",
+    "C#",
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,7 +78,7 @@ class _HomeScreenState extends State<HomeScreen> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Row(
+                const Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Icon(
@@ -39,10 +93,10 @@ class _HomeScreenState extends State<HomeScreen> {
                     )
                   ],
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 20,
                 ),
-                Padding(
+                const Padding(
                   padding: EdgeInsets.only(right: 15, bottom: 3),
                   child: Text(
                     "Hi, Programer",
@@ -53,12 +107,163 @@ class _HomeScreenState extends State<HomeScreen> {
                         letterSpacing: 1),
                   ),
                 ),
+                const SizedBox(
+                  height: 15,
+                ),
                 Container(
-                  
+                  alignment: Alignment.center,
+                  height: 55,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Search here...",
+                        prefixIcon: const Icon(
+                          Icons.search,
+                          size: 28,
+                        ),
+                        hintStyle: TextStyle(
+                          color: Colors.black.withOpacity(0.5),
+                        )),
+                  ),
                 )
               ],
             ),
           ),
+          const SizedBox(
+            height: 15,
+          ),
+          Column(
+            children: [
+              GridView.builder(
+                itemCount: catNames.length,
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  childAspectRatio: 1.5,
+                ),
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: [
+                      Container(
+                        height: 60,
+                        width: 60,
+                        decoration: BoxDecoration(
+                          color: catColors[index],
+                          shape: BoxShape.circle,
+                        ),
+                        child: Center(
+                          child: catIcons[index],
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        catNames[index],
+                        style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black.withOpacity(0.7)),
+                      )
+                    ],
+                  );
+                },
+              ),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Courses",
+                      style:
+                          TextStyle(fontWeight: FontWeight.w600, fontSize: 24),
+                    ),
+                    Text(
+                      "See All",
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                          color: Color(0xff674AEF)),
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              GridView.builder(
+                itemCount: imgList.length,
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio:
+                        (MediaQuery.of(context).size.height - 50 - 25) /
+                            (4 * 240),
+                    mainAxisSpacing: 10,
+                    crossAxisSpacing: 10),
+                itemBuilder: (context, index) {
+                  return InkWell(
+                    onTap: () {},
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Container(
+                        padding:
+                            const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: const Color(0xffF5F3FF)),
+                        child: Column(
+                          children: [
+                            Padding(
+                                padding: EdgeInsets.all(10),
+                                child: Image.asset(
+                                  "Assets/${imgList[index]}.png",
+                                  height: 100,
+                                  width: 100,
+                                )),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                              Text(
+                                imgList[index],
+                                style: TextStyle(
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.black.withOpacity(0.6)
+                                ),
+                              ),
+                            const SizedBox(height: 10,),
+                            Text("55 Videos", style: TextStyle(fontSize: 15,fontWeight: FontWeight.w500,color: Colors.black.withOpacity(0.6)),)
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              )
+            ],
+          )
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        showUnselectedLabels: true,
+        selectedFontSize: 18,
+        unselectedItemColor: Colors.grey,
+        selectedItemColor: Color(0xff674AEF),
+        iconSize: 32,
+
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home),label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.assessment),label: 'Courses'),
+          BottomNavigationBarItem(icon: Icon(Icons.favorite),label: 'Wishlist'),
+          BottomNavigationBarItem(icon: Icon(Icons.person),label: 'Account'),
         ],
       ),
     );
